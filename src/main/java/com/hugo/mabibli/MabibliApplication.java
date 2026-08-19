cc6fce7550bdcf1777fef6c05008b6942e20d960
Hgo92 <hugo_monier@yahoo.fr>
2026-08-19 13:51:18 +0200

    Initialisation

---
+++ src/main/java/com/hugo/mabibli/MabibliApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.mabibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabibliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabibliApplication.class, args);
+	}
+
+}
