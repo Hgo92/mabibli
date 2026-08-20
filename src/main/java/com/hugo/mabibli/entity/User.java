9636477fe1bd0f38ac87bbea2017aab18c96c957
Hgo92 <hugo_monier@yahoo.fr>
2026-08-20 15:39:44 +0200

    ajout de User entity et repository

---
+++ src/main/java/com/hugo/mabibli/entity/User.java
@@ -0,0 +1,43 @@
+package com.hugo.mabibli.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    @com.fasterxml.jackson.annotation.JsonIgnore
+    private String password;
+
+    @Column(name= "created_at", nullable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDate updatedAt;
+
+    public User() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public LocalDate getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDate createdAt) { this.createdAt = createdAt; }
+
+    public LocalDate getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDate updatedAt) { this.updatedAt = updatedAt; }
+}
---
+++ src/main/java/com/hugo/mabibli/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hugo.mabibli.repository;
+
+import com.hugo.mabibli.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existByUsername(String username);
+
+}
