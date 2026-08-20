b97298ea3670fbf93713350b982ccb1fd6837ea5
Hgo92 <hugo_monier@yahoo.fr>
2026-08-20 15:40:58 +0200

    ajout de security config + UserDetails

---
+++ src/main/java/com/hugo/mabibli/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.hugo.mabibli.security;
+import com.hugo.mabibli.entity.User;
+import com.hugo.mabibli.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur introuvable : " + username));
+        return new UserPrincipal(user);
+    }
+}
