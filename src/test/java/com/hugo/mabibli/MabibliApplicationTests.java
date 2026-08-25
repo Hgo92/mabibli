fd1353c984ad2ee0098adcf91ea389ce192baeae
Hgo92 <hugo_monier@yahoo.fr>
2026-08-25 09:33:23 +0200

    test - création du test de migration/Flyway

--- src/test/java/com/hugo/mabibli/MabibliApplicationTests.java
+++ src/test/java/com/hugo/mabibli/MabibliApplicationTests.java
@@ -2,12 +2,23 @@ package com.hugo.mabibli;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
+@Testcontainers
 @SpringBootTest
+@ActiveProfiles("test")
 class MabibliApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static final PostgreSQLContainer postgres =
+			new PostgreSQLContainer("postgres:17-alpine");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
